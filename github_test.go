84804a444d5b28358bfca9170cf7bf787c317cd4
Gregory Vincic <gregoryvse@gmail.com>
2024-11-02 09:27:12 +0100

    Test GithubConf.Login

--- github_test.go
+++ github_test.go
@@ -1,11 +1,28 @@
 package oauth
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"net/http/httputil"
 	"testing"
 
 	"github.com/gregoryv/golden"
 )
+
+func TestGithubConf_Login(t *testing.T) {
+	c := GithubConf{}
+	h := c.Login()
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", http.NoBody)
+	h(w, r)
+
+	resp := w.Result()
+	exp := 303 // redirect to github
+	if got := resp.StatusCode; got != exp {
+		t.Errorf("got %v, expected %v redirect to github", got, exp)
+	}
+}
 
 func Test_tokenURL(t *testing.T) {
 	c := GithubConf{
@@ -13,7 +30,8 @@ func Test_tokenURL(t *testing.T) {
 		ClientSecret: "SEC",
 	}
 	code := "123"
-	exp := "https://github.com/login/oauth/access_token?client_id=CID&client_secret=SEC&code=123"
+	exp := "https://github.com/login/oauth/access_token" +
+		"?client_id=CID&client_secret=SEC&code=123"
 	if got := c.tokenURL(code); got != exp {
 		t.Errorf("tokenURL(%q)\ngot: %s\nexp: %s", code, got, exp)
 	}
