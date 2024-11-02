54d174f4f7df1b488a1523779e417e939065591a
Gregory Vincic <gregoryvse@gmail.com>
2024-11-02 09:14:46 +0100

    Test tokenURL

--- github_test.go
+++ github_test.go
@@ -2,11 +2,14 @@ package oauth
 
 import "testing"
 
-func xTest_tokenURL(t *testing.T) {
-	var c GithubConf
+func Test_tokenURL(t *testing.T) {
+	c := GithubConf{
+		ClientID:     "CID",
+		ClientSecret: "SEC",
+	}
 	code := "123"
-	exp := "https://..."
+	exp := "https://github.com/login/oauth/access_token?client_id=CID&client_secret=SEC&code=123"
 	if got := c.tokenURL(code); got != exp {
-		t.Errorf("tokenURL(%q): %s, expected %s", code, got, exp)
+		t.Errorf("tokenURL(%q)\ngot: %s\nexp: %s", code, got, exp)
 	}
 }
