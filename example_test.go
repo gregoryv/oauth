6f35ee6ee127eb936ca15dad189582aec2f19955
Gregory Vincic <gregoryvse@gmail.com>
2024-11-02 16:18:46 +0100

    Document example

--- example_test.go
+++ example_test.go
@@ -1,7 +1,6 @@
 package oauth_test
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -11,14 +10,16 @@ import (
 func Example_github() {
 	http.Handle("GET /login", github.Login())
 	http.Handle("GET "+github.RedirectPath(), github.Authorize(enter))
-}
-
-func enter(token string, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome!")
+	http.ListenAndServe(":8080", nil)
 }
 
 var github = oauth.Github{
 	ClientID:     os.Getenv("OAUTH_GITHUB_CLIENTID"),
 	ClientSecret: os.Getenv("OAUTH_GITHUB_SECRET"),
 	RedirectURI:  "http://example.com/oauth/fromgithub",
+}
+
+func enter(token string, w http.ResponseWriter, r *http.Request) {
+	// user successfully authenticated, use the token
+	w.Write([]byte("Welcome!"))
 }
