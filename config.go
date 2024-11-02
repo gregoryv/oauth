753dc420ca1fe545b814659ee6f54bf2ba661ac7
Gregory Vincic <gregoryvse@gmail.com>
2024-11-02 07:54:41 +0100

    Document

--- config.go
+++ config.go
@@ -12,19 +12,21 @@ type GithubConf struct {
 	RedirectURI  string
 }
 
-func (c *GithubConf) AuthURL() string {
+// Login returns a handler that redirects to github authorize.
+func (c *GithubConf) Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, c.authURL(), http.StatusSeeOther)
+	}
+}
+
+// authURL returns githubs url for oath oath authorize flow
+func (c *GithubConf) authURL() string {
 	q := url.Values{}
 	q.Set("client_id", c.ClientID)
 	q.Set("redirect_uri", c.RedirectURI)
 	return fmt.Sprintf(
 		"https://github.com/login/oauth/authorize?%s", q.Encode(),
 	)
-}
-
-func (c *GithubConf) Login() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, c.AuthURL(), http.StatusSeeOther)
-	}
 }
 
 // tokenURL returns github url use to get a new token
